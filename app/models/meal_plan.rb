d93a5334533a27f58f2b8c68813f30fd60a24138
Ulsunut <marwa.berriri@hotmail.com>
2026-04-17 10:35:44 +0100

    updated chat form label and placeholder text per PR review

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,6 +1,12 @@
 class MealPlan < ApplicationRecord
+<<<<<<< Updated upstream
   belongs_to :user
 
   has_many :recipes, dependent: :destroy
   has_one :chat, dependent: :destroy
+=======
+  belongs_to :user, foreign_key: "id_users"
+  has_many :recipes, foreign_key: "id_meal_plans", dependent: :destroy
+  has_one :chat, foreign_key: "id_meal_plans", dependent: :destroy
+>>>>>>> Stashed changes
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :meal_plan
+<<<<<<< Updated upstream
   # This line tells Rails NOT to use 'type' for STI
   self.inheritance_column = :_type_disabled
+=======
+>>>>>>> Stashed changes
 end
