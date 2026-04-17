f1d0e3eaa37175835a280291ad07d6848087df2c
Ulsunut <marwa.berriri@hotmail.com>
2026-04-17 10:53:39 +0100

    completed chat form updates

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -1,12 +1,6 @@
 class MealPlan < ApplicationRecord
-<<<<<<< Updated upstream
   belongs_to :user
 
   has_many :recipes, dependent: :destroy
   has_one :chat, dependent: :destroy
-=======
-  belongs_to :user, foreign_key: "id_users"
-  has_many :recipes, foreign_key: "id_meal_plans", dependent: :destroy
-  has_one :chat, foreign_key: "id_meal_plans", dependent: :destroy
->>>>>>> Stashed changes
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,5 @@
 class Recipe < ApplicationRecord
   belongs_to :meal_plan
-<<<<<<< Updated upstream
   # This line tells Rails NOT to use 'type' for STI
   self.inheritance_column = :_type_disabled
-=======
->>>>>>> Stashed changes
 end
